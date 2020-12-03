1a66b880b28401ad467c5d4bab4cd6305af8689d
CthulhuHasNiceCats <a.dumitrescu@st.hanze.nl>
2020-12-03 15:25:19 +0100

    Highlight and pick-up

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform Hold;
+
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = Hold.position;
+        this.transform.parent = GameObject.Find("HoldingPlace").transform;
+    }
+
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Collider>().enabled = true;
+    }
+}
