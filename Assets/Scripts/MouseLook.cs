f5a7b2081ad201ca83e99f7e8517e3538d8fd869
CthulhuHasNiceCats <a.dumitrescu@st.hanze.nl>
2020-12-06 19:19:15 +0100

    This is the fix for third of the problems, I think

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
+    [SerializeField] private string selectableTag = "selection";
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material defaultMaterial;
     public float mouseSensitivity = 100f;
     public Transform playerBody;
-    float xRotation = 0f; 
+    float xRotation = 0f;
+    private Transform _selection;
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +29,34 @@ public class MouseLook : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            selectionThing(selection);
+        }
+
+        void selectionThing(Transform selection)
+        {
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = highlightMaterial;
+                }
+
+                _selection = selection;
+            }
+        }
     }
 }
