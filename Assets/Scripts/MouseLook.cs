a73bca736bbd6cb07d6b13742eed199ae612c726
CthulhuHasNiceCats <a.dumitrescu@st.hanze.nl>
2020-12-07 14:41:07 +0100

    Updates

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -30,33 +30,31 @@ public class MouseLook : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
 
-        if (_selection != null)
-        {
-            var selectionRenderer = _selection.GetComponent<Renderer>();
-            selectionRenderer.material = defaultMaterial;
-            _selection = null;
-        }
-
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
             var selection = hit.transform;
-            selectionThing(selection);
-        }
-
-        void selectionThing(Transform selection)
-        {
-            if (selection.CompareTag(selectableTag))
+            if (_selection.GetInstanceID() != hit.rigidbody.transform.GetInstanceID())
             {
-                var selectionRenderer = selection.GetComponent<Renderer>();
-                if (selectionRenderer != null)
-                {
-                    selectionRenderer.material = highlightMaterial;
-                }
-
-                _selection = selection;
+                var selectionRenderer = _selection.GetComponent<Renderer>();
+                selectionRenderer.material = defaultMaterial;
+                _selection = null;
+                SaveSelectable(selection);
             }
+
+    }
+    void SaveSelectable(Transform selection)
+    {
+        if (selection.CompareTag(selectableTag))
+        {
+            var selectionRenderer = selection.GetComponent<Renderer>();
+            if (selectionRenderer != null)
+            {
+                selectionRenderer.material = highlightMaterial;
+            }
+
+            _selection = selection;
         }
     }
 }
