9d1d6fb24357c2cfc71096e375e1f3209b9baacd
OGdB <58260499+OGdB@users.noreply.github.com>
2020-12-15 22:55:18 +0100

    PointingDirection Line
    Added a visible line showing what you are pointing at. Parameters controlling line length and control are prone to change.
    * Line changes color if you point at a 'tool'

--- Assets/VR scrips/fingerRaycast.cs
+++ Assets/VR scrips/fingerRaycast.cs
@@ -3,33 +3,68 @@
 public class fingerRaycast : MonoBehaviour
 {
     public GameObject startPoint;
+    public float pointerStartWidth;
+    public float pointerEndWidth;
+    public float pointerDistance = 2f;
+    public Color idlePointerColor = Color.blue;
+    public Color pointingPointerColor = Color.green;
+
     private RaycastHit hit;
     private Collider toolPointedAt;
-    private Color rayColor = Color.white;
+    private LineRenderer pointingLine;
+
+    private void Awake()
+    {
+        pointingLine = GetComponent<LineRenderer>();
+    }
 
     private void Update()
     {
-        Debug.DrawRay(startPoint.transform.position, startPoint.transform.forward, rayColor);
-
         CheckIfInteractable();
+        PointingLine();
     }
 
     private void CheckIfInteractable()
     {
         if (Physics.Raycast(startPoint.transform.position, startPoint.transform.forward, out hit, Mathf.Infinity, LayerMask.GetMask("Interactable")))
         {
-            rayColor = Color.green;
-
-            CheckIfTool();
+            if (hit.distance < pointerDistance)
+            {
+                CheckIfTool();
+            }
         }
         else
         {
-            rayColor = Color.red;
-
             ClearHighlight();
         }
     }
+    private void PointingLine()
+    {
+        pointingLine.SetPosition(0, startPoint.transform.position);
 
+        if (toolPointedAt == null)
+        {
+            pointingLine.SetPosition(1, startPoint.transform.position + transform.TransformDirection(Vector3.forward) * pointerDistance);
+
+            pointingLine.material.color = idlePointerColor;
+            pointerStartWidth = 0.005f;
+            pointerEndWidth = 0.005f;
+            pointingLine.startWidth = pointerStartWidth;
+            pointingLine.endWidth = pointerEndWidth;
+        }
+    }
+    private void PointingAtTool()
+    {
+        pointingLine.material.color = pointingPointerColor;
+
+        pointerStartWidth = 0.01f;
+        pointerEndWidth = 0.005f;
+
+        pointingLine.startWidth = pointerStartWidth;
+        pointingLine.endWidth = pointerEndWidth;
+
+        pointingLine.SetPosition(1, toolPointedAt.transform.position);
+    }
     private void CheckIfTool()
     {
         if (hit.collider.CompareTag("Tools"))
@@ -37,6 +72,7 @@ public class fingerRaycast : MonoBehaviour
             if (toolPointedAt == null)
             {
                 toolPointedAt = hit.collider;
+                PointingAtTool();
                 toolPointedAt.GetComponent<Highlightable>().Highlight();
             }
             else if (toolPointedAt.GetInstanceID() != hit.collider.GetInstanceID())
@@ -54,4 +90,5 @@ public class fingerRaycast : MonoBehaviour
             toolPointedAt = null;
         }
     }
+
 }
