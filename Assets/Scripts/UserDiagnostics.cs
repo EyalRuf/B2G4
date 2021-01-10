c0d7a9ac1f33047ba124275766f642f823dba6cd
OGdB <58260499+OGdB@users.noreply.github.com>
2021-01-10 15:41:25 +0100

    merge1 with sten

--- Assets/Scripts/UserDiagnostics.cs
+++ Assets/Scripts/UserDiagnostics.cs
@@ -7,12 +7,27 @@ public class UserDiagnostics : MonoBehaviour
 {
     public SteamVR_Action_Boolean LeftHandY;
     public GameObject diaInterface;
+    public GameObject hydraulicText;
 
     private void Update()
     {
         if (LeftHandY.stateDown)
         {
             diaInterface.SetActive(!diaInterface.activeSelf);
+/*
+            if (hydraulicText.activeSelf)
+            {
+                monki();
+            }*/
         }
     }
+
+    public void monki()
+    {
+        hydraulicText.GetComponent<TextMesh>().text =
+            "Resistance Level = " + Random.Range(-20f, 20f) + "\n" +
+            "Water ionization = " + Random.Range(0f, 20f) + "\n" +
+            "Last filter replacement =";
+    }
 }
+
