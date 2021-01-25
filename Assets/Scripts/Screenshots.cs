be44a00ceb1b60fa4a7a58fb62bbdf26a4dcf394
OGdB <58260499+OGdB@users.noreply.github.com>
2021-01-25 01:37:36 +0100

    Added screenshots

--- Assets/Scripts/Screenshots.cs
+++ Assets/Scripts/Screenshots.cs
@@ -1,19 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.IO;
 using UnityEngine;
 using Valve.VR;
 
+#if (UNITY_EDITOR) 
 public class Screenshots : MonoBehaviour
 {
     public SteamVR_Action_Boolean screenshotBtn;
 
     void Update()
     {
-        //if (screenshotBtn.stateDown)
-        //{
-        //    ScreenCapture.CaptureScreenshot(Application.dataPath + System.DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".png");
-        //    UnityEditor.AssetDatabase.Refresh();
-        //    print("screensht");
-        //}
+        if (screenshotBtn.stateDown)
+        {
+            string filePath = Application.dataPath + "/Screenshots/";
+            string screenshotName = "Screenshot_" + System.DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".png";
+
+            ScreenCapture.CaptureScreenshot(System.IO.Path.Combine(filePath, screenshotName));
+
+            //ScreenCapture.CaptureScreenshot(Application.dataPath + System.DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".png");
+            UnityEditor.AssetDatabase.Refresh();
+            print("Smile!");
+        }
     }
 }
+#endif
